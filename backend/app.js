8bf9f3bbef2bee0ccbf7c892834f60b0a00622f5
lymyr <176290600+lymyr@users.noreply.github.com>
2026-09-13 18:51:15 +0800

    feat: added unconfigured cors

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,14 @@ import express from "express";
 import postRouter from "./routes/postRouter.js";
 import authRouter from "./routes/adminRouter.js";
 import commentRouter from "./routes/commentRouter.js";
+import cors from "cors"
+
 process.loadEnvFile()
 
 const app = express()
+app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({extended: true }))
 app.use("/v1/posts", postRouter)
 app.use("/v1/admin", authRouter)
 app.use("/v1/posts/:postId/comments", commentRouter)
